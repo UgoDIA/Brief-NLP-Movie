1933431c7cc2e616cdb6aa864c621771b6b22f8b
UgoDIA <112081131+UgoDIA@users.noreply.github.com>
2023-10-31 14:50:44 +0100

    Update settings.py

--- backend/src/NLP_app/settings.py
+++ backend/src/NLP_app/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-f-4b#!+1zk*l#jv3#7g==x&=76nfsb0gjecnceyh#7pro74)d(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
